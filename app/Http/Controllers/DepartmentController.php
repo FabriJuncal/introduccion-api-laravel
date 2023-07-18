e014d2a3343d486f408bb8247b5221d004248abb
FabriJuncal <juncalfabri@gmail.com>
2023-07-17 23:41:49 -0300

    Se modificó el controllador de la entidad 'Department'

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -12,7 +12,8 @@ class DepartmentController extends Controller
      */
     public function index()
     {
-        //
+        $department = Department::all();
+        return response()->json($department);
     }
 
     /**
@@ -20,7 +21,22 @@ class DepartmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = ['name' => 'required|string|min:1|max:100'];
+        $validator = \Validator::make($request->input(), $rules);
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()->all()
+            ], 400);
+        }
+
+        $department = new Department($request->input());
+        $department->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Departament created successfully'
+        ], 200);
     }
 
     /**
@@ -28,7 +44,10 @@ class DepartmentController extends Controller
      */
     public function show(Department $department)
     {
-        //
+        return response()->json([
+            'status' => true,
+            'data' => $department
+        ], 200);
     }
 
     /**
@@ -36,7 +55,21 @@ class DepartmentController extends Controller
      */
     public function update(Request $request, Department $department)
     {
-        //
+        $rules = ['name' => 'required|string|min:1|max:100'];
+        $validator = \Validator::make($request->input(), $rules);
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()->all()
+            ], 400);
+        }
+
+        $department->update($request->input());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Departament updated successfully'
+        ], 200);
     }
 
     /**
@@ -44,6 +77,10 @@ class DepartmentController extends Controller
      */
     public function destroy(Department $department)
     {
-        //
+        $department->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Departament deleted successfully'
+        ], 200);
     }
 }
